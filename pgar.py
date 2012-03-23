d91bd403cca0127d37c056df30202a856d6db7e8
Michael Ramirez <ramirezmike2@gmail.com>
2012-03-22 20:12:34 -0400

    Completed

--- pgar.py
+++ pgar.py
@@ -2,14 +2,26 @@ import sys
 import hashlib
 
 MIN_ARGS = 2
+PASSWORD_SIZE = 10
+
+def capitalizeHalf(string):
+	return string[0:5].upper() + string[5:10]
+
+def hashCut(string):
+	return string[0:PASSWORD_SIZE]	
+
+def hash(string):
+	return hashlib.sha256(string).hexdigest()
 
 def main():
+	PASSWORD = hashlib.sha256() 
 	if len(sys.argv) < MIN_ARGS:
 		print 'Missing Arguments'
 		exit(1)
-	apple = sys.argv[1]
-	juice = sys.argv[2]
-	print apple + juice
+	for arg in sys.argv:
+		PASSWORD.update(hash(arg))
+	PASSWORD = capitalizeHalf(hashCut(PASSWORD.hexdigest()))
+	print "Password:" +  PASSWORD
 
 if __name__ == '__main__':
 	main()
